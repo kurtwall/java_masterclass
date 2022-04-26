6be6e4f6d2041b48686ee2218e67e5826f6acf88
Kurt.Wall <kurt.wall@surescripts.com>
2022-04-26 06:37:30 -0700

    Remove stray character

--- FloatAndDouble/src/com/kurtwall/Main.java
+++ FloatAndDouble/src/com/kurtwall/Main.java
@@ -7,7 +7,5 @@ public class Main {
         System.out.println("Max float = " + Float.MAX_VALUE);
         System.out.println("Min double = " + Double.MIN_VALUE);
         System.out.println("Max double = " + Double.MAX_VALUE);
-
-        x
     }
 }
