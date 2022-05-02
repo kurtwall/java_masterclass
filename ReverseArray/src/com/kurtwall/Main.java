249fe2ac804f40420fb8e8f2074b717b52461ebd
Kurt.Wall <kurt.wall@surescripts.com>
2022-05-02 11:25:08 -0700

    Reverse an array

--- ReverseArray/src/com/kurtwall/Main.java
+++ ReverseArray/src/com/kurtwall/Main.java
@@ -5,23 +5,19 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 10, 6, 4};
-        print(array);
+        int[] array = {1, 2, 3, 10, 6, 4};
         reverse(array);
-        print(array);
-    }
-
-    private static void print(int[] array) {
-        System.out.println(Arrays.toString(array));
     }
 
     private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
         int maxIndex = array.length - 1;
         int halfLength = maxIndex / 2;
-        for (int i = 0; i < halfLength ; i++) {
+        for (int i = 0; i < halfLength; i++) {
             int temp = array[i];
             array[i] = array[maxIndex - i];
             array[maxIndex - i] = temp;
         }
+        System.out.println("Reversed array = " + Arrays.toString(array));
     }
 }
